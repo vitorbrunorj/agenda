30961be870429f2a7a8889ab06ddfc24fda96d74
VitorBruno <vitorbrunorj@gmail.com>
2023-09-29 20:13:22 -0300

    Subindo o projeto realizado

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,6 @@
-exports.paginaInicial = (req, res) => {
-  res.render('index', {
-    titulo: 'Este será o título da página',
-    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  });
-  return;
-};
+const Contato = require("../models/ContatoModel");
 
-exports.trataPost = (req, res) => {
-  res.send(req.body);
-  return;
+exports.index = async (req, res) => {
+  const contatos = await Contato.buscaContatos();
+  res.render("index", { contatos });
 };
